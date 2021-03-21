b9fe38e3db8329c584942e90355c5423eccf4bdc
kc <xie981090574@gmail.com>
2021-03-21 10:35:38 +0800

    demo init

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+const path = require('path')
+
+app.use('/css',  express.static(path.join(__dirname, '/css')));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+// This will emit the event to all connected sockets
+io.emit("some event", {
+  someProperty: "some value",
+  otherProperty: "other value",
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  console.log("socket", socket.id);
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+    io.emit("chat message", msg);
+  });
+});
+
+http.listen(3000, () => {
+  console.log("listening on *:3000");
+});
